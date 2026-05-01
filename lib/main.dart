58e4cee670078444d21f661299dc73387a28773a
SunchhaySin <sinsunchhay1995@gmail.com>
2026-05-01 12:37:14 +0700

    added comment about notif function

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:smarthomefridge/pages/loadingPage.dart';
 import 'package:firebase_core/firebase_core.dart';
+// fix the notification function
 
 
 void main() async{
